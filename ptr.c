efc82816c686c623f08dc8fe28ff98d716028ec7
AnishDeore <59721609+AnishDeore@users.noreply.github.com>
2020-01-10 11:21:33 +0530

    Create ptr.c

---
+++ ptr.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+  int arr[5]={10,20,30,40,50};
+  
+  int *ptr=&arr[2];
+  int val;
+  printf("ptr=%p,*ptr=%d\n",ptr,*ptr);
+  
+  ptr=&arr[2];
+  val=*ptr++;   //val=*ptr, ptr=ptr+1
+  printf("val=%d,*ptr=%d,*ptr=%p\n",val,*ptr,ptr);
+    
+  return 0;
+}
