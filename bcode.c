9b6f65047f459440876c86c5272b0054c590f6f8
AnishDeore <59721609+AnishDeore@users.noreply.github.com>
2020-02-01 21:14:20 +0530

    Create bcode.c

---
+++ bcode.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int w=1;
+int e=2;
+int r=8;
+int ans,i,x,t1,t2;
+int main()
+{
+    for(i=0;i<5;i++)
+    {
+     w=e+r;
+     ans=i+w;
+     printf("values of ans:%d\n",ans);
+
+    }
+
+    int a=10;
+    int b=5;
+    if(a=10)
+       {
+        x=a+b;
+        t1=x;
+
+        printf("value of t1:%d \n",t1);
+    }
+    else
+       {
+           int c=a+c;
+       }
+       t2=a*b;
+
+       printf("value of t2:%d",a*b);
+    return 0;
+}
