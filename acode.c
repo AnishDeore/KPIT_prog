3d66bc7af6e6bef1a3397500afc45a12077313c4
AnishDeore <59721609+AnishDeore@users.noreply.github.com>
2020-02-01 21:15:01 +0530

    Create acode.c

---
+++ acode.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int w=1;
+int e=2;
+int r=8;
+int ans,i,x,t1,t2;
+
+int main()
+{   w=e+r;
+    for(i=0;i<5;i++)
+    {
+     ans=i+w;
+     printf("values of ans:%d\n",ans);
+
+    }
+
+    int a=10;
+    int b=5;
+    t1=a+b;
+
+        printf("value of t1:%d \n",t1);
+
+       t2=a*b;
+
+       printf("value of t2:%d",t2);
+    return 0;
+}
